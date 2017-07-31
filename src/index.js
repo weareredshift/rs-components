a70bc6a23b1d6e831606c040d4f04a5f5f2f1e31
Sasha Klein <sashafklein@gmail.com>
2017-07-31 15:16:45 -0700

    Updated webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
 import BaseForm from 'components/BaseForm';
 import Checkbox from 'components/Checkbox';
 import Dropdown from 'components/Dropdown';

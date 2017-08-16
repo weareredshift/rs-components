3983204fbd29975663090babe411126d3fc2a53c
Sasha Klein <sashafklein@gmail.com>
2017-08-16 10:12:33 -0700

    Made InlineSearch a bit more flexible

--- src/components/InlineSearch/index.js
+++ src/components/InlineSearch/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
-import { string, arrayOf, node, shape } from 'prop-types';
+import { string, arrayOf, node, shape, func } from 'prop-types';
 
 /**
  * Searches through given options for relevant results, and presents first match inline.
@@ -8,7 +8,9 @@ import { string, arrayOf, node, shape } from 'prop-types';
  * @param {Object} props
  * @param {string} props.className
  * @param {Object[]} props.options Array of option objects through which to search
- * @param {React.Component} props.options[].image Optional node (usually image) to display before search
+ * @param {React.Component} props.options[].left Optional node (usually image) to display left of result
+ * @param {React.Component} props.options[].right Optional node (usually image) to display right of result
+ * @param {React.Component} props.options[].replacement Optional node (usually image) to display in place of result
  * @param {string} props.options[].name Name to search through
  */
 export class InlineSearchUC extends React.Component {
@@ -18,34 +20,44 @@ export class InlineSearchUC extends React.Component {
   }
 
   foundText(find, regex) {
+    const { cursor } = this.props;
     const { selected, focused } = this.state;
 
     const split = find.name.split(regex);
 
+    const result = find.replacement
+      ? find.replacement
+      : (
+        <span>
+          { split[0] }
+          <span className="inlinesearch__matching-text">{ split[1] }</span>
+          { split.slice(2) }
+        </span>
+      );
+
     return (
       <span
-        className={ classnames('inlinesearch__result', selected && 'inlinesearch__fake-results--selected') }
+        className={ classnames('inlinesearch__result inlinesearch__result--found', selected && 'inlinesearch__fake-results--selected') }
       >
-        { find.image && <span className="inlinesearch__image">{ find.image }</span> }
-        { split[0] }
-        <span className="inlinesearch__matching-text">{ split[1] }</span>
-        { split.slice(2) }
-        { !split[2] && focused && <span className="inlinesearch__cursor">|</span> }
+        { find.left && <span className="inlinesearch__left">{ find.left }</span> }
+        { result }
+        { find.right && <span className="inlinesearch__right">{ find.right }</span> }
+        { focused && cursor }
       </span>
     );
   }
 
   textWithoutMatch () {
-    const { placeholder } = this.props;
+    const { placeholder, cursor } = this.props;
     const { search, focused } = this.state;
 
     return (
-      <span className="inlinesearch__noresult">
+      <span className="inlinesearch__result inlinesearch__result--empty">
         { search || focused ?
             search :
             <span className="inlinesearch__placeholder">{ placeholder }</span>
         }
-        { focused && <span className="inlinesearch__cursor">|</span> }
+        { focused && cursor }
       </span>
     );
   }
@@ -53,6 +65,12 @@ export class InlineSearchUC extends React.Component {
   focus() {
     if (this.el) { this.el.focus(); }
     this.setState({ focused: true });
+  }
+
+  handleKeyPress (item, key) {
+    if (key === 'Enter') {
+      item.onAction && item.onAction(item);
+    }
   }
 
   render () {
@@ -68,7 +86,7 @@ export class InlineSearchUC extends React.Component {
           type="text"
           ref={ el => { this.el = el; } }
           className={ classnames('inlinesearch__input', focused && 'inlinesearch__input--focused') }
-          onKeyPress={ e => { this.handleKeypress(e.target.value); } }
+          onKeyPress={ e => { this.handleKeyPress(firstFind, e.key); } }
           value={ search }
           onChange={ e => { this.setState({ search: e.target.value }); } }
           onBlur={ () => { this.setState({ focused: false }); } }
@@ -81,7 +99,6 @@ export class InlineSearchUC extends React.Component {
             ? this.foundText(firstFind, searchRegex)
             : this.textWithoutMatch()
           }
-
         </div>
       </div>
     );
@@ -91,10 +108,18 @@ export class InlineSearchUC extends React.Component {
 InlineSearchUC.propTypes = {
   className: string,
   options: arrayOf(shape({
-    name: string,
-    image: node
+    name: string.isRequired,
+    left: node,
+    right: node,
+    replacement: node,
+    onAction: func
   })),
-  placeholder: string
+  placeholder: string,
+  cursor: node
+};
+
+InlineSearchUC.defaultProps = {
+  cursor: <span className="inlinesearch__cursor">|</span>
 };
 
 export default InlineSearchUC;

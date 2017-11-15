931629bf7b580cc1e9543f0ec22e0a7e827b095d
Daniel <danbernardi1@gmail.com>
2017-11-15 14:40:12 -0800

    Adds optional className prop to tooltip

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -6,6 +6,7 @@ import { setActiveTooltip } from './actions';
 /**
  * Renders a tooltip component
  * @param {Object} props                    React props object
+ * @param {string} props.className          Optional prop to pass to tooltip
  * @param {string} props.id                 String defining tooltips unique id
  * @param {Object} props.triggerContent     React DOM element wrapped by the trigger
  * @param {Object} props.children           React element wrapped by tooltip component, revealed when tooltip is active
@@ -15,7 +16,7 @@ import { setActiveTooltip } from './actions';
  * @returns {React.Component}               Returns a react component
  */
 export function TooltipUC (props) {
-  const { id, children, dispatch, openTooltipID, triggerContent } = props;
+  const { id, children, dispatch, openTooltipID, triggerContent, className } = props;
 
   const toggleTooltip = () => {
     const nextTooltip = openTooltipID === id ? null : id;
@@ -23,7 +24,7 @@ export function TooltipUC (props) {
   };
 
   return (
-    <div className="tooltip">
+    <div className={ `tooltip ${className ? className : ''}` }>
       <div className={ `tooltip__trigger` } onClick={ () => toggleTooltip() }>
         { triggerContent && triggerContent }
       </div>
@@ -37,7 +38,8 @@ TooltipUC.propTypes = {
   triggerContent: PropTypes.object,
   children: PropTypes.element,
   dispatch: PropTypes.func,
-  openTooltipID: PropTypes.string
+  openTooltipID: PropTypes.string,
+  className: PropTypes.string
 };
 
 const mapStateToProps = state => ({
